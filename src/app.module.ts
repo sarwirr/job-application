024874301d4f7329f984be886eb2e3d60e60262e
sarwir <sarrah.benhalima@insat.ucar.tn>
2023-07-24 10:49:55 +0200

    mongo configuration setted and created user class

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
+require('dotenv').config(); 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot(
+      process.env.MONGODB_KEY,
+    ),],
   controllers: [AppController],
   providers: [AppService],
 })
